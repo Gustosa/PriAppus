7939c087ac589d26f83350082a1dac9e816db7ac
Gustosa <gustavomartins5820@gmail.com>
2023-10-13 15:53:11 -0300

    coisas que faltaram

---
+++ js/teste.js
@@ -0,0 +1,36 @@
+const questoes = [
+    {
+        numero: 1,
+        enunciado: "Non consectetur dolore ut exercitation ipsum dolor enim aute tempor sint tempor reprehenderit deserunt culpa.Id ex consequat anim dolor amet sint ipsum culpa aute pariatur irure nisi voluptate consequat.Reprehenderit ipsum culpa dolore Lorem incididunt nisi enim ex non excepteur sint proident.Nostrud in magna dolor voluptate.Irure ullamco non officia duis.Elit aliquip sunt cillum ut ipsum ad mollit nisi non irure anim voluptate esse.",
+        alternativas: [
+            {
+                letra: "a",
+                opcao: "Eiusmod occaecat deserunt consequat labore ipsum ea.",
+                status: false
+            },
+            {
+                letra: "b",
+                opcao: "Eiusmod occaecat deserunt consequat labore ipsum ea.",
+                status: true
+            },
+            {
+                letra: "c",
+                opcao: "Eiusmod occaecat deserunt consequat labore ipsum ea.",
+                status: false
+            },
+            {
+                letra: "d",
+                opcao: "Eiusmod occaecat deserunt consequat labore ipsum ea.",
+                status: false
+            },
+            {
+                letra: "e",
+                opcao: "Eiusmod occaecat deserunt consequat labore ipsum ea.",
+                status: false
+            }
+        ]
+    },
+    {
+
+    }
+]
