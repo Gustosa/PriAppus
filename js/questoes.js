9bd2910801231fb6bbaead58ac1758566dca47a0
Gustosa <gustavomartins5820@gmail.com>
2023-10-26 16:18:38 -0300

    ducto deferente e organização de HTMl

--- js/questoes.js
+++ js/questoes.js
@@ -1,17 +1,35 @@
 function Corrigir() {
     var alternativas = document.querySelectorAll('.alternativa');
-    var resposta_correta, resposta_usuario;
+    var resposta_usuario = [], resposta_correta = [];
 
     alternativas.forEach(element => {
-        if (element.classList.contains('correto')) {
-            resposta_correta = element;
+
+        if (element.children[0].checked) {
+            resposta_usuario.push(element);
         }
 
-        if (element.checked) {
-            resposta_usuario = element;
+        if (element.classList.contains('correta')) {
+            resposta_correta.push(element);
+        }
+    })
+
+    resposta_correta.forEach(resposta => {
+        resposta.classList.add('verde');
+    })
+
+    resposta_usuario.forEach(resposta => {
+        if (resposta.classList.contains('verde')) {
+            return;
+        } else {
+            resposta.classList.add('vermelho');
         }
     })
 
 
 
+    /*if (resposta_usuario === resposta_correta) {
+        resposta_usuario.classList.add("verde");
+    } else {
+        resposta_usuario.classList.add("vermelho");
+    }*/
 }
